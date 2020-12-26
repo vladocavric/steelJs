87fa4952defbcbadcada37366525bb140fca3524
Vlado <vlado.cavric@etondigital.com>
2020-12-26 22:51:31 +0100

    sign up

--- ecom/index.js
+++ ecom/index.js
@@ -3,7 +3,8 @@ const express = require('express')
 const app = express();
 const ejs = require('ejs')
 const bodyParser = require('body-parser')
-const {addUser} = require('./users')
+// const {addUser} = require('./users')
+const usersRepo = require('./repositories/users')
 
 
 app.set('view engine', 'ejs');
@@ -15,14 +16,19 @@ app.get('/', (req, res) => {
 })
 
 app.get('/sign-up', (req, res) => {
-    // console.log(req.body)
-    
     res.render('signUp');
 })
 
-app.post('/sign-up', (req, res) => {
-    addUser(req.body.email, req.body.password)  
-    res.redirect('/');
+app.post('/sign-up', async (req, res) => {
+    const {email, password, confirmPassword} = req.body
+    const existingUser = await usersRepo.getOneBy({email})
+    if (existingUser) {
+        res.send('Email already in use')
+    } 
+    if (password !== confirmPassword) {
+        res.send('Please check password')
+    }
+    res.send('Account created')
 })
 
 app.get('/sign-in', (req, res) => {
--- ecom/repositories/users.js
+++ ecom/repositories/users.js
@@ -1,10 +1,87 @@
+const fs = require('fs')
+const crypto = require('crypto')
 class UsersRepositories {
     constructor(filename) {
         if (!filename) {
             throw new Error('Createing a new repository requires a filename')
         }
         this.filename = filename
-     }
+        try {
+            fs.accessSync(this.filename)
+        } catch {
+            fs.writeFileSync(this.filename, '[]') 
+        }
+    }
+    async getAll() {
+        // Open the file called this.filename, parse the contents and return
+        try{
+            return JSON.parse(await fs.promises.readFile(this.filename, {encoding: 'utf8'}))
+        } catch {
+            console.log('nesto je poslo po zlu')
+        }
+    }
+    async getOne(id) {
+        const records = await this.getAll()
+        return records.find(record => record.id === id)
+    }
+
+    async getOneBy(filters) {
+        const records = await this.getAll()
+        for (let record of records) {
+            let found = true
+            for (let key in filters) {
+                if (record[key] !== filters[key]) {
+                    found = false
+                }
+            }
+            return found ?  record : undefined
+            // if (found) {
+            //     return record
+            // }
+        }
+    }
+
+    async create(attributes) {
+        attributes.id = this.randomId()
+        const records = await this.getAll()
+        await records.push(attributes)
+        this.writeAll(records)
+    }
+
+    async update(id, attributes) {
+        const records = await this.getAll()
+        const record = records.find(record => record.id === id)
+        if (!record) {
+            throw new Error(`There is no user with this id:${id}`)
+        }
+        attributes.id = id
+        Object.assign(record, attributes)
+        this.writeAll(records)
+
+
+        // moze i ovako
+        // const index = await records.findIndex(record => record.id === id)
+        // attributes.id = records[index].id
+        // index >= 0 ? await records.splice(index, 1, attributes) : null   
+        // this.writeAll(records)
+    }
+    async delete(id) {
+        const records = await this.getAll()
+        const filteredRecords = records.filter(record => record.id !== id)
+        this.writeAll(filteredRecords)
+        
+        
+        // can be doen and this way
+        // const index = await records.findIndex(record => record.id === id)
+        // index >= 0 ? await records.splice(index, 1) : null       
+        // this.writeAll(records)
+    }
+    randomId() {
+        return crypto.randomBytes(12).toString('hex')
+    }
+    async writeAll(records) {
+        await fs.promises.writeFile(this.filename, JSON.stringify(records, null, 4))
+    }
 }
 
-new UsersRepositories()
+module.exports = new UsersRepositories('users.json')
